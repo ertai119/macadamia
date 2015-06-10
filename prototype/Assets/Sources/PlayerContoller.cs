ed9b11fd9c241bbf7e5e526197b14d01ba8400c3
ertai119 <ertai119@naver.com>
2015-06-11 08:26:31 +0900

    #00 prototype

---
+++ prototype/Assets/Sources/PlayerContoller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerContoller : MonoBehaviour
+{
+	float speed = 5;
+	float tilt = 5;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float dirX = Input.GetAxis("Horizontal");
+		float dirY = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (dirX, 0, dirY);
+
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0, 0, GetComponent<Rigidbody> ().velocity.x * -tilt);
+	}
+}
---
+++ prototype/Assets/Sources/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+
+	float tumble = 5;
+
+	void Start()
+	{
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
