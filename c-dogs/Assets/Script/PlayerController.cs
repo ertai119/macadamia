06476297c946b0f642b3266e76866fe11388dc33
ertai119 <ertai119@naver.com>
2016-05-25 09:17:24 +0900

    #00

---
+++ c-dogs/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour {
+
+	Vector3 velocity;
+	Rigidbody myRigidBody;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myRigidBody = GetComponent<Rigidbody> ();
+	}
+
+	public void Move(Vector3 vel)
+	{
+		velocity = vel;
+	}
+
+	public void LookAt(Vector3 lookPoint)
+	{
+		Vector3 heightCorrentedPoint = new Vector3 (lookPoint.x, transform.position.y, lookPoint.z);
+		transform.LookAt (heightCorrentedPoint);
+	}
+
+	public void FixedUpdate()
+	{
+		myRigidBody.MovePosition (myRigidBody.position + velocity * Time.fixedDeltaTime);
+	}
+}
