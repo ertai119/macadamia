708350553039a2927e843a6099486343c83aa445
ertai119 <ertai119@naver.com>
2016-05-26 08:07:51 +0900

    #01 gun controller

---
+++ c-dog/Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(PlayerController))]
+[RequireComponent (typeof(GunController))]
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 5;
+
+	Camera viewCamera;
+	PlayerController playerController;
+	GunController gunController;
+
+	// Use this for initialization
+	void Start () 
+	{
+		playerController = GetComponent<PlayerController> ();
+		gunController = GetComponent<GunController> ();
+
+		viewCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Movement Input
+		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));	
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		playerController.Move (moveVelocity);
+
+		// Lookat Input
+		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+
+		float rayDistance;
+		if (groundPlane.Raycast(ray, out rayDistance))
+		{
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.DrawLine(ray.origin, point, Color.red);
+			playerController.LookAt(point);
+		}
+
+		// Weapon Input
+		if (Input.GetMouseButton(0))
+		{
+			gunController.Shoot ();	
+		}
+	}
+}
