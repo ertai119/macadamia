458710ed0321c5d190793bd4bf2853c9b0817141
ertai119 <ertai119@naver.com>
2016-05-30 23:20:49 +0900

    #00 title banner

--- c-dog/Assets/Script/GameUI.cs
+++ c-dog/Assets/Script/GameUI.cs
@@ -8,10 +8,34 @@ public class GameUI : MonoBehaviour {
     public Image fadePlane;
     public GameObject gameOverUI;
 
-	void Start ()
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
+
+    Spawner spawner;
+
+    void Start ()
     {
         FindObjectOfType<Player>().OnDeath += OnGameOver;
-	}
+    }
+
+    void Awake()
+    {
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        newWaveTitle.text = "- Wave : #" + waveNumber + " -";
+
+        string strEnemyCount = spawner.waves[waveNumber - 1].infinite == true
+            ? "Infinite" : spawner.waves[waveNumber - 1].enemyCount + "";
+        newWaveEnemyCount.text = " Enemies : " + strEnemyCount;
+
+        StopCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
+    }
 
     void OnGameOver()
     {
@@ -19,8 +43,37 @@ public class GameUI : MonoBehaviour {
         gameOverUI.SetActive(true);
 	}
 
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float delayTime = 1f;
+        float speed = 3f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+            
+        while (animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-100, 250, animatePercent);
+            yield return null;
+        }
+    }
+
     IEnumerator Fade(Color from, Color to, float time)
     {
+        Cursor.visible = true;
+
         float speed = 1 / time;
         float percent = 0;
 
@@ -30,6 +83,7 @@ public class GameUI : MonoBehaviour {
             fadePlane.color = Color.Lerp(from, to, percent);
             yield return null;
         }
+
     }
 
     // UI input
